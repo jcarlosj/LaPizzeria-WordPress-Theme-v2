84fd9e0c575ee9a9e6599a63cf2385841339a2e4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-02-15 14:45:44 -0500

    [+] functions.php: CDN Normalize, CDN Google Fonts

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@
  */
 
 function pizzashop_register_styles () {
-    wp_enqueue_style( 'style', get_stylesheet_uri(), [], filemtime( untrailingslashit( get_stylesheet_directory() ) .'/style.css' ), 'all' );
+    wp_enqueue_style( 'normalize', 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css', [], '8.0.1', 'all' );
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:wght@400;700;900&display=swap', [], '1.0.0' ); 
+    wp_enqueue_style( 'style', get_stylesheet_uri(), [ 'normalize' ], filemtime( untrailingslashit( get_stylesheet_directory() ) .'/style.css' ), 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'pizzashop_register_styles' );
