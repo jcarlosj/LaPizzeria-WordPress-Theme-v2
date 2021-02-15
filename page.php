7f62e817ed2cd8250177b7755be6b7e9c62b50c8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-02-15 13:33:17 -0500

    [.] page.php

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php
+/** Page Template File 
+ * @package PizzaShop
+ */
+?>
+ 
+<?php while ( have_posts() ) : the_post(); ?>
+
+    <div class="container file-name">
+        <span>
+            <?php esc_html_e( basename( __FILE__ ) ); ?>
+        </span>
+    </div>
+    
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+
+<?php endwhile; ?>
+        
+    
