cbd87ec061f60cd3de1beba25a60622960406e38
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-02-15 11:49:37 -0500

    [.] WP-Theme: Define, +Screenshot, +Assets

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La Pizzería</title>
+</head>
+<body>
+    <h1>Theme - La Pizzería</h1>
+    <p>Desarrollada para Gutenberg</p>
+</body>
+</html>
