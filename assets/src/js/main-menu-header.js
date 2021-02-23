89bb9d00efe296aaded6bc6d6951d97b150a76a4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-02-22 19:44:08 -0500

    [+] JavaScript: Cambia icono del menu & Cierra menu (breakpoint-medium)

--- assets/src/js/main-menu-header.js
+++ assets/src/js/main-menu-header.js
@@ -4,13 +4,14 @@ const
 
 let displayMenu = false;
 
-const medium = 768;
+const breakpointMedium = 768;
 
 menuIcon .addEventListener( 'click', () => {
     displayMenu = ! displayMenu;
+    
     if( displayMenu ) {
         menuIcon .classList .add( 'change-icon' );   
-        menuHeader .classList .remove( 'not-display' ); 
+        menuHeader .classList .remove( 'not-display' );
     }
     else {
         menuIcon .classList .remove( 'change-icon' );
@@ -19,5 +20,19 @@ menuIcon .addEventListener( 'click', () => {
     
     console.log( `Display menu ${ ! displayMenu }` );
 });
+
+/** Seguimiento al cambio de la ventana del navegador */
+window .onresize = function () {
+    const windowWidth = document .documentElement .clientWidth;     //  Obtiene el ancho interno del elemento
+
+    console .log( `${ windowWidth }px` );
+
+    /** Valida si el ancho interno del elemento es mayor a el breakpoint de tamano medium (768px) */
+    if( breakpointMedium < windowWidth ) {
+        menuIcon .classList .remove( 'change-icon' );   
+        menuHeader .classList .add( 'not-display' );   
+        displayMenu = false;
+    }
+}
 
 console .log( 'Main Header' );
